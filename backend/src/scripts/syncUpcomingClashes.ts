be8afdf58952f229e64d11ffc1d51ce9538dc4d5
Dijamg <dijam.goran@hotmail.com>
2025-07-15 02:46:23 +0300

    small fix

--- backend/src/scripts/syncUpcomingClashes.ts
+++ backend/src/scripts/syncUpcomingClashes.ts
@@ -5,23 +5,30 @@ export const syncUpcomingClashes = async () => {
     console.log('Syncing upcoming clashes');
     try {
         const currentClashes = await TournamentService.getAllUpcomingClashTournaments();
+
         const upcomingClashes = await RiotService.getUpcomingClashTournaments();
-           
-         //If there are current clashes, delete all current clashes and add all upcoming clashes
-         if(currentClashes.length > 0) {
-            console.log(`Deleting all current clashes and adding all upcoming clashes`);
-            for (const clash of currentClashes) {
+
+        const existingIds = new Set(currentClashes.map(c => c.id));
+
+        //clashes that are not in the database
+        const newClashes = upcomingClashes.filter(clash => !existingIds.has(clash.id));
+
+        // We want to delete all clashes that are not in the upcomingClashes array
+        const upcomingClashIds = new Set(upcomingClashes.map(c => c.id));
+
+        for (const clash of currentClashes) {
+            if (!upcomingClashIds.has(clash.id)) {
                 await TournamentService.deleteUpcomingClashTournament(clash.id);
             }
         }
-
-        //Add all upcoming clashes
-        for (const clash of upcomingClashes) {
+        // we want to add all new clashes to the database
+        for (const clash of newClashes) {
             if(clash.schedule.length > 0) {
                 const startTime = new Date(clash.schedule[0].startTime);
                 await TournamentService.addUpcomingClashTournament(clash.themeId, clash.nameKey, clash.nameKeySecondary, startTime);
             }
         }
+
         console.log(`Synced upcoming clashes`);
     } catch (error) {
         console.error('Error fetching members:', error);
